0ecb3470c15ea32b9bd04a471906d976531d04bc
Pentagon3000 <harry.filis1999@gmail.com>
2021-09-14 11:27:14 +0300

    Analog Temperature sensor Test and Temp Monitoring in LCD - Next Update AC control with IR led

---
+++ Analog_TemperatureSensor_Test/RTE/_Target_1/Pre_Include_Global.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HW477Test.Target_1' 
+ * Target:  'Target_1' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil::Device:STM32Cube HAL:Common:1.13.0 */
+#define USE_HAL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ Analog_TemperatureSensor_Test/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,105 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HW477Test.Target_1' 
+ * Target:  'Target_1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube HAL:ADC:1.13.0 */
+#define RTE_DEVICE_HAL_ADC
+/* Keil::Device:STM32Cube HAL:CAN:1.13.0 */
+#define RTE_DEVICE_HAL_CAN
+/* Keil::Device:STM32Cube HAL:COMP:1.13.0 */
+#define RTE_DEVICE_HAL_COMP
+/* Keil::Device:STM32Cube HAL:CRC:1.13.0 */
+#define RTE_DEVICE_HAL_CRC
+/* Keil::Device:STM32Cube HAL:Common:1.13.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.13.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DAC:1.13.0 */
+#define RTE_DEVICE_HAL_DAC
+/* Keil::Device:STM32Cube HAL:DFSDM:1.13.0 */
+#define RTE_DEVICE_HAL_DFSDM
+/* Keil::Device:STM32Cube HAL:DMA:1.13.0 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:EXTI:1.13.0 */
+#define RTE_DEVICE_HAL_EXTI
+/* Keil::Device:STM32Cube HAL:FIREWALL:1.13.0 */
+#define RTE_DEVICE_HAL_FIREWALL
+/* Keil::Device:STM32Cube HAL:GPIO:1.13.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:HCD:1.13.0 */
+#define RTE_DEVICE_HAL_HCD
+/* Keil::Device:STM32Cube HAL:I2C:1.13.0 */
+#define RTE_DEVICE_HAL_I2C
+/* Keil::Device:STM32Cube HAL:IRDA:1.13.0 */
+#define RTE_DEVICE_HAL_IRDA
+/* Keil::Device:STM32Cube HAL:IWDG:1.13.0 */
+#define RTE_DEVICE_HAL_IWDG
+/* Keil::Device:STM32Cube HAL:LCD:1.13.0 */
+#define RTE_DEVICE_HAL_LCD
+/* Keil::Device:STM32Cube HAL:LPTIM:1.13.0 */
+#define RTE_DEVICE_HAL_LPTIM
+/* Keil::Device:STM32Cube HAL:MMC:1.13.0 */
+#define RTE_DEVICE_HAL_MMC
+/* Keil::Device:STM32Cube HAL:NAND:1.13.0 */
+#define RTE_DEVICE_HAL_NAND
+/* Keil::Device:STM32Cube HAL:NOR:1.13.0 */
+#define RTE_DEVICE_HAL_NOR
+/* Keil::Device:STM32Cube HAL:OPAMP:1.13.0 */
+#define RTE_DEVICE_HAL_OPAMP
+/* Keil::Device:STM32Cube HAL:PCD:1.13.0 */
+#define RTE_DEVICE_HAL_PCD
+/* Keil::Device:STM32Cube HAL:PWR:1.13.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:QSPI:1.13.0 */
+#define RTE_DEVICE_HAL_QSPI
+/* Keil::Device:STM32Cube HAL:RCC:1.13.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:RNG:1.13.0 */
+#define RTE_DEVICE_HAL_RNG
+/* Keil::Device:STM32Cube HAL:RTC:1.13.0 */
+#define RTE_DEVICE_HAL_RTC
+/* Keil::Device:STM32Cube HAL:SAI:1.13.0 */
+#define RTE_DEVICE_HAL_SAI
+/* Keil::Device:STM32Cube HAL:SD:1.13.0 */
+#define RTE_DEVICE_HAL_SD
+/* Keil::Device:STM32Cube HAL:SMBUS:1.13.0 */
+#define RTE_DEVICE_HAL_SMBUS
+/* Keil::Device:STM32Cube HAL:SPI:1.13.0 */
+#define RTE_DEVICE_HAL_SPI
+/* Keil::Device:STM32Cube HAL:SRAM:1.13.0 */
+#define RTE_DEVICE_HAL_SRAM
+/* Keil::Device:STM32Cube HAL:SWPMI:1.13.0 */
+#define RTE_DEVICE_HAL_SWPMI
+/* Keil::Device:STM32Cube HAL:Smartcard:1.13.0 */
+#define RTE_DEVICE_HAL_SMARTCARD
+/* Keil::Device:STM32Cube HAL:TIM:1.13.0 */
+#define RTE_DEVICE_HAL_TIM
+/* Keil::Device:STM32Cube HAL:TSC:1.13.0 */
+#define RTE_DEVICE_HAL_TSC
+/* Keil::Device:STM32Cube HAL:UART:1.13.0 */
+#define RTE_DEVICE_HAL_UART
+/* Keil::Device:STM32Cube HAL:USART:1.13.0 */
+#define RTE_DEVICE_HAL_USART
+/* Keil::Device:STM32Cube HAL:WWDG:1.13.0 */
+#define RTE_DEVICE_HAL_WWDG
+/* Keil::Device:Startup:1.3.1 */
+#define RTE_DEVICE_STARTUP_STM32L4XX    /* Device Startup for STM32L4 */
+
+
+#endif /* RTE_COMPONENTS_H */
